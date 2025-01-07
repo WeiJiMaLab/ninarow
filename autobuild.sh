fbda92bcf4540b2b42278460ca8a78e94c1e19d5
Jordan Lei <thunderflash.lei@gmail.com>
2025-01-07 13:55:50 -0500

    Using Boost instead of std for random number generation

--- autobuild.sh
+++ autobuild.sh
@@ -10,18 +10,26 @@ case $env in
         echo "Installing dependencies for Mac..."
         brew install cmake
         brew install swig
+        brew install boost
         ;;
     2)
         echo "Installing dependencies for Cluster..."
         apt-get install cmake
         apt-get install swig
+        apt-get install libboost-all-dev
         ;;
     3)
-        echo "Please install cmake using the Windows installer, and download and unzip SWIG to a directory."
+        echo "Please install CMake using the Windows installer, and download and unzip SWIG to a directory."
         echo "Add the following environment variables for all users:"
         echo "- SWIG_DIR : <path to unzipped SWIG directory>"
         echo "- SWIG_EXECUTABLE : <path to swig.exe in unzipped SWIG directory>"
         echo "Finally, add the SWIG directory (SWIG_DIR above) to your PATH system variable."
+
+        echo "To install Boost on Windows:"
+        echo "- Download Boost from https://www.boost.org/ and extract it (e.g., C:\\Boost)."
+        echo "- Add an environment variable BOOST_DIR pointing to the Boost directory (e.g., C:\\Boost)."
+        echo "- Add the Boost 'lib' directory (e.g., C:\\Boost\\lib) to your PATH."
+        echo "CMake will detect Boost automatically if BOOST_DIR is set correctly."
         ;;
     *)
         echo "Invalid option. Exiting."
