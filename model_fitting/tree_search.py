59dfc21c0e71476fff8154daf5aeefe5b00e3fb2
Jordan Lei <thunderflash.lei@gmail.com>
2025-11-24 13:04:14 -0500

    changing init points

--- model_fitting/tree_search.py
+++ model_fitting/tree_search.py
@@ -55,12 +55,12 @@ class TreeSearch:
 
         # Control parameters (search behavior)
         self.parameter_list = [
-            {"name": "pruning_threshold", "initial_value": 1.8, "lower_bound": 0.1, "upper_bound": 10.0, "plausible_lower_bound": 1.0, "plausible_upper_bound": 4.0},
-            {"name": "stopping_prob", "initial_value": 0.01, "lower_bound": 0.001, "upper_bound": 1.0, "plausible_lower_bound": 0.01, "plausible_upper_bound": 0.2},
-            {"name": "feature_drop", "initial_value": 0.25, "lower_bound": 0, "upper_bound": 1, "plausible_lower_bound": 0, "plausible_upper_bound": 0.4},
-            {"name": "lapse_rate", "initial_value": 0.05, "lower_bound": 0.01, "upper_bound": 1, "plausible_lower_bound": 0.05, "plausible_upper_bound": 0.2},
-            {"name": "opp_scale", "initial_value": 2.2, "lower_bound": 0.25, "upper_bound": 4, "plausible_lower_bound": 1.2, "plausible_upper_bound": 3},
-            {"name": "center_weight", "initial_value": 0.5, "lower_bound": -10, "upper_bound": 10, "plausible_lower_bound": -3, "plausible_upper_bound": 3},
+            {"name": "pruning_threshold", "initial_value": 2.3, "lower_bound": 0.1, "upper_bound": 10.0, "plausible_lower_bound": 1.0, "plausible_upper_bound": 4.0},
+            {"name": "stopping_prob", "initial_value": 0.05, "lower_bound": 0.001, "upper_bound": 1.0, "plausible_lower_bound": 0.01, "plausible_upper_bound": 0.2},
+            {"name": "feature_drop", "initial_value": 0.25, "lower_bound": 0, "upper_bound": 1, "plausible_lower_bound": 0.01, "plausible_upper_bound": 0.4},
+            {"name": "lapse_rate", "initial_value": 0.05, "lower_bound": 0, "upper_bound": 1, "plausible_lower_bound": 0.05, "plausible_upper_bound": 0.2},
+            {"name": "opp_scale", "initial_value": 2.3, "lower_bound": 0.25, "upper_bound": 4, "plausible_lower_bound": 1.2, "plausible_upper_bound": 3},
+            {"name": "center_weight", "initial_value": 0.2, "lower_bound": -10, "upper_bound": 10, "plausible_lower_bound": -3, "plausible_upper_bound": 3},
         ]
 
         # Feature templates and weights (modular design)
@@ -76,8 +76,8 @@ class TreeSearch:
                 "initial_value": self.initial_weights[group],
                 "lower_bound": -5,
                 "upper_bound": 15,
-                "plausible_lower_bound": self.initial_weights[group] - 3,
-                "plausible_upper_bound": self.initial_weights[group] + 3,
+                "plausible_lower_bound": self.initial_weights[group] - 2,
+                "plausible_upper_bound": self.initial_weights[group] + 2,
             })
 
         # Extract parameter arrays for optimization
