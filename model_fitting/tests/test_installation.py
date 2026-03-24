62cf58ae558b74672b4e4051a0682352e7fa4bd8
Jordan Lei <thunderflash.lei@gmail.com>
2026-03-24 11:13:04 -0400

    removing unnecessary files

--- model_fitting/tests/test_installation.py
+++ model_fitting/tests/test_installation.py
@@ -1,5 +1,7 @@
-# your other imports here ...
 import sys, os
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 from fourbynine import fourbynine_board, fourbynine_pattern, fourbynine_move, Player_Player1, Player_Player2, bool_to_player, player_to_string
 
 print("Initial Python Installation Test Successful!")
